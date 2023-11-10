e97e5588acdd8f3e9d90c49ad2bbdf447f0f65ab
dc-neo <deepu.cr@gmail.com>
2023-11-10 15:42:39 -0500

    fix the predict button to actually work

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import sys
 import logging
 import pickle
+from sklearn.preprocessing import StandardScaler
 model_filename="./heart_attack"
 import numpy as np
 # Initalise the Flask app
@@ -30,11 +31,20 @@ def predict():
     f.close()
     all_cols=X_test
     input_df=pd.DataFrame(msg_data,columns=all_cols,index=[0])
-    app.logger.warning("test log", input_df)
-    cat_cols = ['sex', 'cp', 'fbs', 'restecg', 'exng', 'slp', 'caa', 'thall']
-    con_cols = ['age', 'trtbps', 'chol', 'thalachh', 'oldpeak']
+    req_cols = ["sex_0", "cp_0", "cp_2", "cp_3", "fbs_0", "restecg_0", "restecg_2", "exng_1", "slp_0", "slp_2", "caa_0", "caa_2", "caa_3", "caa_4", "thall_0", "thall_2", "thall_3", "sex_1", "cp_1", "fbs_1", "restecg_1", "exng_0", "slp_1", "caa_1", "thall_1" ]
+    cat_cols = ["sex", "cp", "fbs", "restecg", "exng", "slp", "caa", "thall"]
+    con_cols = ["age", "trtbps", "chol", "thalachh", "oldpeak"]
     ha_encoded_data = pd.get_dummies(input_df, columns = cat_cols)
-    app.logger.warning("test log", input_df)
+    ha_encoded_data = ha_encoded_data.reindex(columns=req_cols, fill_value=0)
+    # instantiating the scaler
+    scaler = StandardScaler()
+    # scaling the continuous features
+    app.logger.warning("1:", input_df)
+    app.logger.warning("2:", ha_encoded_data)
+    ha_encoded_data = ha_encoded_data.join(input_df[con_cols])
+    ha_encoded_data[con_cols] = scaler.fit_transform(ha_encoded_data[con_cols])
+    app.logger.warning("1:", input_df)
+    app.logger.warning("2:", ha_encoded_data)
     model=load_model(model_filename)
     arr_results = model.predict(ha_encoded_data)
     treatment_likelihood=""
